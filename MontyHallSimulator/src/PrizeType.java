740a5081db1bc4828c33f1ecf2974db2296e039e
Mrhamon11 <aviamon11@gmail.com>
2025-01-23 00:36:37 -0500

    removed string field from PriceType enum

--- MontyHallSimulator/src/PrizeType.java
+++ MontyHallSimulator/src/PrizeType.java
@@ -1,14 +1,7 @@
 public enum PrizeType {
-    STANDARD_GOAT1("STANDARD_GOAT1"),
-    STANDARD_GOAT2("STANDARD_GOAT2"),
-    CAR("CAR"),
-    GOOD_GOAT("GOOD_GOAT"),
-    BAD_GOAD("BAD_GOAD");
-
-    private String name;
-
-    private PrizeType(String name)
-    {
-        this.name = name;
-    }
+    STANDARD_GOAT1(),
+    STANDARD_GOAT2(),
+    CAR(),
+    GOOD_GOAT(),
+    BAD_GOAD()
 }
