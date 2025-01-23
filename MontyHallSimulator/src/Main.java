dd8aeaf40f3665493547a5604b90b94147dea2e9
Mrhamon11 <aviamon11@gmail.com>
2025-01-23 00:27:14 -0500

    updated formatting

--- MontyHallSimulator/src/Main.java
+++ MontyHallSimulator/src/Main.java
@@ -10,11 +10,12 @@ public class Main {
         simulator2.runSimulation();
 
         System.out.println("Standard Monty Hall Scenario odds:");
+        System.out.println();
         System.out.println("Switch Doors false: " + simulator1.getResults());
         System.out.println();
         System.out.println("Switch Doors true: " + simulator2.getResults());
 
-        System.out.println();
+        System.out.println("_______________________________________________________________________________\n");
 
         IMontyHallScenario scenario2 = new GoodGoatMontyHallScenario();
         MontyHallSimulator simulator3 = new MontyHallSimulator(scenario2, 1000000, false);
@@ -24,6 +25,7 @@ public class Main {
         simulator4.runSimulation();
 
         System.out.println("Good Goat Monty Hall Scenario odds:");
+        System.out.println();
         System.out.println("Switch Doors false: " + simulator3.getResults());
         System.out.println();
         System.out.println("Switch Doors true: " + simulator4.getResults());
